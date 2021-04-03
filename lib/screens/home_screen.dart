5f6b108cf720fc4f2728545c05e240d1f08c1597
chico3434 <chicokarkow@hotmail.com>
2021-04-03 13:33:30 -0300

    Record implementado

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:desvie/global.dart';
 import 'package:desvie/screens/game_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,18 +9,77 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  SharedPreferences prefs;
+  int record;
+
+  @override
+  void initState() {
+    _getRecord();
+    super.initState();
+  }
+
+  _getRecord() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+      record = prefs.getInt('record') ?? 0;
+    });
+  }
+
+  _saveRecord() async {
+    await prefs.setInt('record', record);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Desvie'),
       ),
-      body: Center(
+      body: Container(
+        padding: EdgeInsets.all(12),
+        alignment: Alignment.center,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Sua pontuação'),
-            Text('${Global.score}'),
+            Card(
+              elevation: 3,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(20),
+                  alignment: Alignment.center,
+                  child: Column(
+                    children: [
+                      Text('Seu record'),
+                      Text(
+                        '$record',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  )),
+            ),
+            Card(
+              elevation: 3,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(20),
+                alignment: Alignment.center,
+                child: Column(
+                  children: [
+                    Text('Sua pontuação'),
+                    Text(
+                      '${Global.score}',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ),
             ElevatedButton(
               onPressed: () async {
                 await Navigator.of(context).push(
@@ -28,10 +88,18 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 );
                 setState(() {
-                  Global.score = Global.score;
+                  if (record < Global.score) {
+                    setState(() {
+                      record = Global.score;
+                    });
+                    _saveRecord();
+                  }
                 });
               },
-              child: Text('Iniciar'),
+              child: Container(
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  child: Text('Iniciar')),
             ),
           ],
         ),
